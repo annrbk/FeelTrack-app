7bf4b09295616a853fc7bec4622ba365cf68e4e0
Hanna Rabushka <anyachagina@mail.ru>
2026-05-28 20:20:00 +0300

    add typography setup

--- app/App.tsx
+++ app/App.tsx
@@ -6,8 +6,36 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import BottomSheet from "../components/BottomSheet";
 import { ThemeProvider } from "../context/ThemeContext";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
---
+++ styles/typography.ts
@@ -0,0 +1,6 @@
+export const typography = {
+  regular: { fontFamily: "Montserrat_400Regular" },
+  medium: { fontFamily: "Montserrat_500Medium" },
+  semiBold: { fontFamily: "Montserrat_600SemiBold" },
+  bold: { fontFamily: "Montserrat_700Bold" },
+};
