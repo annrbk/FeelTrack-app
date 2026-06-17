0750c4ba6430e6fa9e04905c87af8ab5d71c2af2
Hanna Rabushka <anyachagina@mail.ru>
2026-06-17 11:39:31 +0300

    upload new user profile avatars

--- constants/avatars.ts
+++ constants/avatars.ts
@@ -1,4 +1,6 @@
 export const avatars = [
   { id: "1", image: require("../assets/avatars/avatar_1.png") },
   { id: "2", image: require("../assets/avatars/avatar_2.png") },
+  { id: "3", image: require("../assets/avatars/avatar_3.png") },
+  { id: "4", image: require("../assets/avatars/avatar_4.png") },
 ];
