60dfa7ee79584313f8f6fab623f637bcb5ae7338
Hanna Rabushka <anyachagina@mail.ru>
2026-08-26 13:59:10 +0300

    add i18n translation to Button and Loading components

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,7 @@
 import { Pressable, View, Text } from "react-native";
 import { getStyles } from "../styles/SignInScreen";
 import { useAppStyle } from "../hooks/useAppStyle";
+import { useTranslation } from "react-i18next";
 
 export type ButtonProps = {
   handleSubmit: () => void;
@@ -8,6 +9,8 @@ export type ButtonProps = {
 
 export default function Button({ handleSubmit }: ButtonProps) {
   const { styles } = useAppStyle(getStyles);
+  const { t } = useTranslation();
+
   return (
     <View style={styles.buttonContainer}>
       <Pressable
@@ -16,7 +19,9 @@ export default function Button({ handleSubmit }: ButtonProps) {
           handleSubmit();
         }}
       >
-        <Text style={styles.buttonText}>Sign up</Text>
+        <Text style={styles.buttonText}>
+          {t("signUpScreen.signUpButtonText")}
+        </Text>
       </Pressable>
     </View>
   );
--- components/Loading.tsx
+++ components/Loading.tsx
@@ -3,15 +3,18 @@ import { ActivityIndicator, Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { useAppStyle } from "../hooks/useAppStyle";
 import { getStyles } from "../styles/Loading.styles";
+import { useTranslation } from "react-i18next";
 
 export default function Loading() {
   const { colors, styles } = useAppStyle(getStyles);
+
+  const { t } = useTranslation();
 
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
         <ActivityIndicator size="large" color={colors.btnPrimary} />
-        <Text style={styles.text}>Loading...</Text>
+        <Text style={styles.text}>{t("loadingScreen.loadingText")}</Text>
       </SafeAreaView>
     </SafeAreaProvider>
   );
