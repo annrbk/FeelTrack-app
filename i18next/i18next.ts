0cf7efd952fef95f7c9c222fee44261d766b219e
Hanna Rabushka <anyachagina@mail.ru>
2026-09-01 17:47:33 +0300

    load saved language from AsyncStorage

--- i18next/i18next.ts
+++ i18next/i18next.ts
@@ -1,6 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import { getLocales } from "expo-localization";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import en from "./locales/en";
 import be from "./locales/be";
 import ru from "./locales/ru";
@@ -8,20 +9,34 @@ import { setupCalendarLocale } from "./calendarLocales";
 
 export const lng = getLocales()[0].languageCode ?? "en";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en,
-    be,
-    ru,
+const languageStorage = {
+  type: "languageDetector" as const,
+  async: true,
+  detect: async (callback: (lang: string) => void) => {
+    try {
+      const savedLanguage = await AsyncStorage.getItem("language");
+      if (savedLanguage) {
+        setupCalendarLocale(savedLanguage);
+        return callback(savedLanguage);
+      }
+    } catch (error) {
+      console.error("Error reading language from AsyncStorage:", error);
+    }
+    setupCalendarLocale(lng);
+    callback(lng);
   },
-  lng,
-  fallbackLng: "en",
-  supportedLngs: ["en", "be", "ru"],
-  interpolation: {
-    escapeValue: false,
-  },
-});
+  init: () => {},
+  cacheUserLanguage: () => {},
+};
 
-setupCalendarLocale(lng);
+i18n
+  .use(languageStorage)
+  .use(initReactI18next)
+  .init({
+    resources: { en, be, ru },
+    fallbackLng: "en",
+    supportedLngs: ["en", "be", "ru"],
+    interpolation: { escapeValue: false },
+  });
 
 export default i18n;
